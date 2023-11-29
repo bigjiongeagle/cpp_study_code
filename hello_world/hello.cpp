b231a2621f02d5634c6136743003cf219c3b600e
bigjiong <123845393+bigjiongeagle@users.noreply.github.com>
2023-11-29 03:12:27 +0000

    hello world upload

---
+++ hello_world/hello.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Author: bigjiong 123845393+bigjiongeagle@users.noreply.github.com
+ * @Date: 2023-11-29 03:07:29
+ * @LastEditors: bigjiong 123845393+bigjiongeagle@users.noreply.github.com
+ * @LastEditTime: 2023-11-29 03:10:47
+ * @FilePath: /cpp_study_code/hello_world/hello.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
