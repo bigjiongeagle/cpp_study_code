a202dd51ea43053f885c173c460de49c9d3b34e3
bigjiong <123845393+bigjiongeagle@users.noreply.github.com>
2023-11-29 06:49:22 +0000

    test code upload

---
+++ simple_test/escape.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello\tWorld\n\n" << endl;
+    return 0;
+}
---
+++ simple_test/function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void func(void);
+
+static int count = 10;
+
+int main()
+{
+    while (count --)
+    {
+        func();
+    }
+    
+    return 0;
+}
+
+void func(void)
+{
+    static int i = 5;
+    i++;
+    cout << "i is " << i << " and count is " << count << endl; 
+}
